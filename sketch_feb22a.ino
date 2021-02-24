bd15a90bed00ef26899d8d374d06f1e67586462e
Nikin Baidar <nikinbaidarr@gmail.com>
2021-02-24 13:42:53 +0545

    it is kinda working

--- sketch_feb22a.ino
+++ sketch_feb22a.ino
@@ -3,18 +3,16 @@
 int trigger[2] = {10,8};
 int echo[2] = {11,9};
 
-/* For data manupulation */
+/* init 1 */
 
 float temperature = (17) + 273.15; // Celcius + 273.15
 
 float speedOfSound;
 
 float initialDistance[2];
-float distance[2];
+float distance[2][2];
 
-String sequence = "";
-int sequenceNumber;
-unsigned visitorCount = 0;
+unsigned visitorCount;
 
 int i; // gloabal loop variable
 
@@ -24,29 +22,28 @@ void setup() {
   delay(200);
 
   speedOfSound = 20.05 * sqrt(temperature) / 10000; // centimeters per microseconds
-  
-  for(i = 0; i < 2; i++) {
+
+  for (i = 0 ; i < 2; i++) {
     pinMode(trigger[i],OUTPUT);
     pinMode(echo[i],INPUT);
     initialDistance[i] = measureDistance(i);
   }
-
-  Serial.print("System has been initialized\n");
+  
+  visitorCount = 0; 
+  
+  Serial.print("System init complete\n");
 }
 
 void loop() {
 
-  for(i = 0; i < 2; i++) {
-    distance[i] = measureDistance(i);
-    Serial.print("distance[");Serial.print(i);Serial.print("] = ");Serial.print(distance[i]);Serial.print(" cm ");
-  }  
-
-  visitorCount = countPeople(visitorCount);
-
-  Serial.print("\nvisitors = "); Serial.print(visitorCount); Serial.print("\n");
-        
-  delay(800);
-
+  if (obstacleDetected()) {
+    if (isEntering())
+      visitorCount++;
+    else if (isExiting())   
+      visitorCount--;      
+  }
+      
+  Serial.print("visitors = "); Serial.print(visitorCount); Serial.print("\n");    
 }
 
 float measureDistance(int i) {
@@ -56,53 +53,25 @@ float measureDistance(int i) {
     digitalWrite(trigger[i],HIGH);
     delayMicroseconds(10);
     digitalWrite(trigger[i],LOW);
-    delayMicroseconds(2);
 
   return (pulseIn(echo[i],HIGH)/2*speedOfSound);
 }
 
-unsigned countPeople(unsigned visitorCount) {
-
-  if (distance[0] < distance[1]) {
-    if (distance[0] < (0.80 * initialDistance[0]) && sequence.charAt(0) != '1') 
-      sequence += '1';
-    if (distance[1] < (0.80 * initialDistance[1]) && sequence.charAt(0) != '2') 
-      sequence += '2';
+bool obstacleDetected() {
+  for (i = 0 ; i < 2; i++) {
+    distance[0][i] = measureDistance(i);
+    delay(200);
+    distance[1][i] = measureDistance(i);
   }
-    
-  else if (distance[1] > distance[0]) {
-    
-    if (distance[1] < (0.80 * initialDistance[1]) && sequence.charAt(0) != '2') 
-      sequence += '2';  
-    if (distance[0] < (0.80 * initialDistance[0]) && sequence.charAt(0) != '1') 
-      sequence += '1';
-  }
-    
-  sequenceNumber = sequence.toInt();
-  
-  Serial.print(sequence); Serial.print("\n");
-
-  switch (sequenceNumber){
-
-    default: {
-      sequence = ""; 
-      break;
-    } 
-      
-    case 12: {
-      visitorCount++;
-      sequence = "";
-      break;
-    }
-
-    case 21:{
-      visitorCount--;
-      sequence = "";
-      break;
-    }
-    
-  }
-  
-  return visitorCount;
-  
+  return (distance[0][0]< 0.77*initialDistance[0] || distance[0][1] < 0.77*initialDistance[1]);
 }
+
+
+bool isEntering(){
+  return  (distance[0][0] > distance[1][0]);
+}
+
+bool isExiting(){
+  return  (distance[0][1] > distance[1][1]);
+}
+  
